bbe7147dc8b057cbfc3a465d8fa753aa1c4e2224
Mohd Khairi <khairi@labs.my>
2015-03-24 00:12:29 +0800

    update assets

--- app/assets/javascripts/material/btn.js
+++ app/assets/javascripts/material/btn.js
@@ -1,6 +1,7 @@
 // btn active
-	$('.btn').on('mousedown touchstart', function() {
+	$(document).on('mousedown touchstart', '.btn', function() {
 		var $this = $(this);
+
 		if (!$this.hasClass('btn-active')) {
 			$this.addClass('btn-active');
 			setTimeout(function() {
--- app/assets/javascripts/material/content.js
+++ app/assets/javascripts/material/content.js
@@ -1,14 +1,12 @@
 // fixed left/right hand side column affix
-	var contentFixPush,
-	    contentFixPushIni;
-
-	if ($('.content').length) {
-		contentFixPushIni = parseInt($('.content').css('padding-bottom').replace('px', ''));
-	};
+	var footerHeight = 0,
+	    htmlHeight = 0;
 
 	$(window).on('scroll', function() {
-		$('.content-fixable').each(function(index) {
-			contentFix($(this));
+		$('.content-fix').each(function(index) {
+			if ($(this).outerHeight() < $(this).closest('.row-fix').outerHeight()) {
+				contentFix($(this));
+			}
 		});
 	});
 
@@ -17,7 +15,6 @@
 			if (!content.hasClass('fixed')) {
 				if ((content.is('[class*="col-xx"]')) || (content.is('[class*="col-xs"]') && $(window).width() >= 480) || (content.is('[class*="col-sm"]') && $(window).width() >= 768) || (content.is('[class*="col-md"]') && $(window).width() >= 992) || (content.is('[class*="col-lg"]') && $(window).width() >= 1440)) {
 					content.addClass('fixed');
-					$('.content-fix-inner', content).css('padding-bottom', contentFixPush).scrollTop(0);
 					$('.content-fix-wrap', content).scrollTop(0);
 				};
 			};
@@ -25,16 +22,17 @@
 			content.removeClass('fixed');
 			$('.content-fix-inner', content).css('padding-bottom', '');
 		}
+
+		var scrolled = $(window).height() + window.pageYOffset;
+
+		if (htmlHeight <= scrolled) {
+			$('.content-fix-inner', content).css('padding-bottom', scrolled - htmlHeight);
+		};
 	}
 
 // fixed left/right hand side column padding bottom and width
 	function contentFixPushCal() {
-		$('.content-fix').each(function(index) {
-			$(this).removeClass('content-fixable');
-			if ($(this).outerHeight() < $(this).closest('.row-fix').outerHeight()) {
-				$(this).addClass('content-fixable');
-			}
-		});
+		htmlHeight = $('body').height();
 
 		$('.content-fix-scroll').each(function(index) {
 			$(this).css('width', $(this).closest('.content-fix').outerWidth());
@@ -42,8 +40,6 @@
 		});
 
 		if ($('.footer').length) {
-			contentFixPush = contentFixPushIni + $('.footer').outerHeight();
-		} else {
-			contentFixPush = contentFixPushIni;
+			footerHeight = $('.footer').outerHeight();
 		}
 	}
