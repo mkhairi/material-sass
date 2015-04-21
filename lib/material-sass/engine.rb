30d05b0b1865ce5af72d7f14e70e49e7d7971532
mkhairi <khairi@labs.my>
2015-04-21 12:39:28 +0800

    remove font-awesome and fix font-path

--- lib/material-sass/engine.rb
+++ lib/material-sass/engine.rb
@@ -5,7 +5,7 @@ module Material
         %w(stylesheets javascripts fonts images).each do |sub|
           app.config.assets.paths << root.join('app/assets', sub).to_s
         end
-        #app.config.assets.precompile << %r(\.(?:eot|svg|ttf|woff2?)$)
+        app.config.assets.precompile << %r(\.(?:eot|svg|ttf|woff2?)$)
       end
     end
   end
