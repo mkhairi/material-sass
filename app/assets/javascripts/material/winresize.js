a5e1d533b8fc0389d62c1362e4dbe4baaa228320
mkhairi <khairi@labs.my>
2015-04-28 11:14:48 +0800

    update assets

--- app/assets/javascripts/material/winresize.js
+++ app/assets/javascripts/material/winresize.js
@@ -1,4 +1,6 @@
-// window resize
+// window smart resize
+	function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
+
 	on_resize(function() {
 		// fixed left/right hand side column padding bottom and width
 			if ($('.content-fix').length) {
@@ -11,11 +13,18 @@
 		// header height
 			headerHeightCal();
 
+		// header nav positioning
+			if ($('.header-nav-scroll').length) {
+				headerNavPos();
+			};
+
 		// tab switch
 			$('.tab-nav').each(function() {
 				tabSwitch($('.nav > li.active', $(this)), null);
 			});
 
-		// tile animation in view
-			tileAnimationInView();
+		// tile in view
+			if ($('.tile-wrap-animation:not(.isinview)').length) {
+				tileInView();
+			};
 	})();
