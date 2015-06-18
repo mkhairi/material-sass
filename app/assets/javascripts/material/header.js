a0cfba8162be539f4bdcecff3b858e13d85798d2
mkhairi <khairi@labs.my>
2015-06-18 17:30:37 +0800

    update assets

--- app/assets/javascripts/material/header.js
+++ app/assets/javascripts/material/header.js
@@ -4,55 +4,19 @@
 	    headerNavMinWidth = 0;
 
 // header affix
-    function runHeaderAffix() {
-	var $header = $('.header');
-        $(window).on('scroll', function() {
-            if ($('.header').length) {
-                if (window.pageYOffset > headerHeight) {
-                    $header.addClass('fixed');
-                } else {
-                    $header.removeClass('fixed');
-                }
-            };
-        });
-    }
-    
+	$(window).on('scroll', function() {
+		if ($('.header').length) {
+			if (window.pageYOffset > headerHeight) {
+				$header.addClass('fixed');
+			} else {
+				$header.removeClass('fixed');
+			}
+		};
+	});
+
 // header height
 	headerHeightCal = function () {
 		if ($('.header').length) {
 			headerHeight = $header.height();
 		};
 	}
-
-// header nav positioning
-    function runHeaderNavPos() {
-        if ($('.header-nav-scroll').length) {
-            $('.header-nav-scroll .nav > li').each(function(index) {
-                var $this = $(this);
-
-                if (index < 3) {
-                    headerNavMinWidth += $this.width();
-                } else {
-                    return false;
-                }
-            });
-        };
-    }
-
-	headerNavPos  = function () {
-		var $headerNav = $('.header-nav-scroll');
-
-		$headerNav.removeClass('pull-down');
-
-		if ($headerNav.width() < headerNavMinWidth) {
-			$headerNav.addClass('pull-down');
-		} else {
-			$headerNav.removeClass('pull-down');
-		}
-	}
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        runHeaderAffix();
-        runHeaderNavPos();
-    }, false);
-    
--- app/assets/javascripts/material/webfont.js
+++ app/assets/javascripts/material/webfont.js
@@ -1,48 +1,45 @@
 // webfont.js
 	WebFontConfig = {
 		classes: false,
-		fontactive: function() {
-			$('.avoid-fout').each(function(index) {
-				$(this).addClass('avoid-fout-done');
-			});
-
-			// fixed left/right hand side column padding bottom and width
-				contentFixPushCal();
-
-			// footer push
-				footerPush();
-
-			// header height
-				headerHeightCal();
-
-			// tile wrap animation
-				tileInView();
-		},
-		fontinactive: function() {
-			$('.avoid-fout').each(function(index) {
-				$(this).addClass('avoid-fout-done');
-			});
-
-			// fixed left/right hand side column padding bottom and width
-				contentFixPushCal();
-
-			// footer push
-				footerPush();
-
-			// header height
-				headerHeightCal();
-
-			// tile wrap animation
-				tileInView();
-		},
 		google: {
 			families: ['Roboto:300,300italic,400,400italic,700,700italic']
-		}
+		},
+		// callbacks
+			active: function() {
+				$('.avoid-fout').each(function(index) {
+					$(this).addClass('avoid-fout-done');
+				});
+
+				// fixed left/right hand side column padding bottom and width
+					contentFixPushCal();
+
+				// footer push
+					footerPush();
+
+				// header height
+					headerHeightCal();
+
+				// tab indicator
+					$('.tab-nav').each(function() {
+						tabSwitch($('.nav > li.active', $(this)), null);
+					});
+
+				// tile wrap animation
+					tileInView();
+			},
+			inactive: function() {
+				$('.avoid-fout').each(function(index) {
+					$(this).addClass('avoid-fout-done');
+				});
+
+				// tile wrap animation
+					tileInView();
+			}
 	};
 
 	(function() {
 		var wf = document.createElement('script');
-		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.10/webfont.js';
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
 		wf.type = 'text/javascript';
 		wf.async = 'true';
 		var s = document.getElementsByTagName('script')[0];
