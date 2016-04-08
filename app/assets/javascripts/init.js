1bffafd2f60bf31d1ce1192f4cfcd9821c6c50fc
mkhairi <khairi@labs.my>
2016-04-08 11:45:54 +0800

    update assets

--- app/assets/javascripts/init.js
+++
@@ -1,9 +0,0 @@
-//onDOMload
-document.addEventListener('DOMContentLoaded', function() {
-   Waves.init({duration: 900});
-}, false);
-
-//turbolink fix
-$(document).on('ready page:change', function() {
-    Waves.init({duration: 900});
-});
