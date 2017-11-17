5099da77291917a1c5f2cae0f69c5cba25621510
Mohd Khairi Mohd Adnan <khairi@labs.my>
2017-11-17 11:53:51 +0800

    remove unused assets

--- lib/material-sass/engine.rb
+++ lib/material-sass/engine.rb
@@ -2,11 +2,9 @@ module Material
   module Sass
     class Engine < ::Rails::Engine
       initializer 'material-sass.assets.precompile' do |app|
-        %w(stylesheets javascripts fonts images).each do |sub|
+        %w(stylesheets javascripts).each do |sub|
           app.config.assets.paths << root.join('app/assets', sub).to_s
         end
-        app.config.assets.precompile << %r(material-icons/MaterialIcons-Regular\.(?:eot|svg|ttf|woff|woff2?)$)
-        app.config.assets.precompile << %r(roboto/Roboto-[\w-]+\.(?:eot|svg|ttf|woff|woff2?)$)
        end
     end
   end
