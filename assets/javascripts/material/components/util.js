629e0c666a3ad286d312f1e4d0e60a1106a38b2c
Mohd Khairi Mohd Adnan <khairi@labs.my>
2018-01-12 15:25:57 +0800

    update assets

--- assets/javascripts/material/components/util.js
+++ assets/javascripts/material/components/util.js
@@ -8,13 +8,6 @@ var Util = function ($) {
   var MAX_UID = 1000000;
   var transition = false;
 
-  var TransitionEndEvent = {
-    WebkitTransition: 'webkitTransitionEnd',
-    MozTransition: 'transitionend',
-    OTransition: 'oTransitionEnd otransitionend',
-    transition: 'transitionend'
-  };
-
   function getSpecialTransitionEndEvent() {
     return {
       bindType: transition.end,
@@ -70,17 +63,9 @@ var Util = function ($) {
       return false;
     }
 
-    var el = document.createElement('material');
-
-    for (var name in TransitionEndEvent) {
-      if (el.style[name] !== undefined) {
-        return {
-          end: TransitionEndEvent[name]
-        };
-      }
-    }
-
-    return false;
+    return {
+      end: 'transitionend'
+    };
   }
 
   var Util = {
