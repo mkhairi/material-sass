e208afd20da5939e62357499858a32a280b9396c
Mohd Khairi Mohd Adnan <khairi@labs.my>
2018-01-24 12:48:34 +0800

    update sprockets

--- assets/javascripts/material-sprockets.js
+++ assets/javascripts/material-sprockets.js
@@ -1,6 +1,2 @@
 //= require_tree ./material/components
-//= require ./material/addons/picker
-//= require ./material/addons/picker.date
-//= require ./material/addons/textarea-autosize
-//= require ./material/addons/waves
-//= require_tree ./material/initializers
+//= require ./material/addons/pickadate
